9ef709cf92c2170de37fda3ccca2a7bf2f8381d1
artherx <xxartherxx@gmail.com>
2023-11-14 16:30:01 -0500

    ahi queda mijo

--- Assets/conteo.cs
+++ Assets/conteo.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class conteo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject ascensor;
+   private bool insideTrigger = false;
+    private float tiempoDentro = 0f;
+
+    private void Update()
     {
-        
+        if (insideTrigger)
+        {
+            tiempoDentro += Time.deltaTime;
+            Debug.Log("Tiempo dentro del trigger: " + tiempoDentro + " segundos");
+        }
+        if(tiempoDentro>=4f && tiempoDentro <8)
+        {
+            transform.Rotate(0,0,1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // Asegúrate de ajustar la etiqueta según tus necesidades
+        {
+            insideTrigger = true;
+            Debug.Log("El cubo ha entrado en el trigger.");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Asegúrate de ajustar la etiqueta según tus necesidades
+        {
+            insideTrigger = false;
+            tiempoDentro = 0f; // Reiniciar el tiempo cuando el cubo sale del trigger
+            Debug.Log("El cubo ha salido del trigger.");
+        }
     }
 }
